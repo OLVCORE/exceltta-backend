3cd65c70f07c8c8cdc828dde7911b5fbf3323af8
OLVCORE <olvsistemas@olvinternacional.com.br>
2025-05-06 13:03:10 -0300

    Delete openai.js
    Substituição de arquivos para atualização OpenAI

--- openai.js
+++
@@ -1,9 +0,0 @@
-// openai.js
-const { OpenAI } = require("openai");
-const config = require("./config");
-
-const openai = new OpenAI({
-  apiKey: config.openaiApiKey
-});
-
-module.exports = openai;
