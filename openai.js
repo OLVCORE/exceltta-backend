f37a00bae91b568257364e61235076347ac53946
OLVCORE <olvsistemas@olvinternacional.com.br>
2025-05-06 12:54:46 -0300

    Update openai.js
    fix: corrigindo configuração do OpenAI

--- openai.js
+++ openai.js
@@ -1,10 +1,9 @@
-const { Configuration, OpenAIApi } = require('openai');
-const config = require('./config');
+// openai.js
+const { OpenAI } = require("openai");
+const config = require("./config");
 
-const configuration = new Configuration({
-  apiKey: config.openaiApiKey,
+const openai = new OpenAI({
+  apiKey: config.openaiApiKey
 });
-
-const openai = new OpenAIApi(configuration);
 
 module.exports = openai;
